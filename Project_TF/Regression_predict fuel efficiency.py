1aacd1976ca1485e6c7f8518dff0ca8158d46c9a
Yubaba0315 <376504500@qq.com>
2019-07-27 15:53:46 +0800

    Tensorflow学习：Fashion_mnint图像基本分类

---
+++ Project_TF/Regression_predict fuel efficiency.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pathlib
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
+
+# Get the data
+# First download the dataset.
+
+dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+dataset_path
+
+
+# Import it using pandas
+
+column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
+                'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                      na_values = "?", comment='\t',
+                      sep=" ", skipinitialspace=True)
+dataset = raw_dataset.copy()
+dataset.tail()
